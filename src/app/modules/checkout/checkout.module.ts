8a67223a08c739838099dbb9e3d93717daca0e8b
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-07-06 19:48:09 +1000

    US6: add products service and tests.

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { CheckoutComponent } from './checkout.component';
+import { ProductsService } from './services/products/products.service';
 
 
 
 @NgModule({
   declarations: [CheckoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [
+    ProductsService
   ]
 })
 export class CheckoutModule { }
