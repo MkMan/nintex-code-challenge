5f06b1fe36505f3445413f7b9f7747cd8a29faf7
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-07-07 12:38:38 +1000

    US8: add implementation for order service.

--- src/app/modules/checkout/services/order/order.service.ts
+++ src/app/modules/checkout/services/order/order.service.ts
@@ -11,6 +11,30 @@ export class OrderService {
   private order: Order;
 
   public updateOrder(id: string, qty: number): Order {
+    let productIndex: number;
+
+    this.order.products.forEach((product, index) => {
+      if (product.id === id) {
+        productIndex = index;
+        return;
+      }
+    });
+
+    if (productIndex >= 0) {
+      // the product already has an entry in the order
+      if (qty === 0) {
+        // remove it because the new qty is 0
+        this.order.products.splice(productIndex, 1);
+      } else {
+        // amend the qty
+        this.order.products[productIndex].qty = qty;
+      }
+
+    } else {
+      // the product is new to the order, add it
+      this.order.products.push({ id, qty });
+    }
+
     return this.order;
   }
 }
